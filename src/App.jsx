f00853725778c81eee79860a716c85293c26eac6
Pranil Dhutraj <pranildhutraj@gmail.com>
2024-08-11 19:35:35 +0530

    vercel routing bug fixing

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter , Route, Routes } from "react-router-dom";
 import FlashcardList from "./components/FlashCardList";
 import Dashboard from "./components/Dashboard";
 import "./App.css";
 
 const App = () => {
   return (
-    <Router>
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<FlashcardList />} />
         <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
-    </Router>
+    </BrowserRouter>
   );
 };
 
